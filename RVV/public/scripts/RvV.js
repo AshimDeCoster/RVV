4228c8a5c44ec76ecc853f4cc1da4bd58b8e721b
AshimDeCoster <ashim.de.coster@student.howest.be>
2014-12-28 14:54:54 +0100

    navigatie fade

--- RVV/public/scripts/RvV.js
+++ RVV/public/scripts/RvV.js
@@ -5,6 +5,22 @@ $(document).ready(function () {
     });
 });
 
+window.onscroll = function (event) {
+    var scroll = parseInt($(window).scrollTop());
+    
+    if(scroll > 80) 
+        $('nav').addClass("f-nav");
+    
+    else
+        $('nav').removeClass("f-nav");
+    
+
+    console.log("" + $(window).scrollTop());
+    //if (scroll > 0 && scroll < 5) { $('#over').goTo(); console.log("nu" + ($('#over').offsetTop()));}
+    //if (scroll == ($('#over').scrollTop() + 5)) { $('#Parcours').goTo(); }
+    //$('#over').goTo();
+}
+
 
 
 function ScrollNaarOver() {    
