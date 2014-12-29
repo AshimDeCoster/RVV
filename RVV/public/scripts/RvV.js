70d72742c5d2e699213bc0242ea5253077ac72c6
glennVw <glenn.vanwynsberghe@student.howest.be>
2014-12-29 17:15:08 +0100

    parcours tijdlijn aanpassingen

--- RVV/public/scripts/RvV.js
+++ RVV/public/scripts/RvV.js
@@ -32,12 +32,6 @@ window.onscroll = function (event) {
     
     else
         $('nav').removeClass("f-nav");
-    
-
-    console.log("" + $(window).scrollTop());
-    //if (scroll > 0 && scroll < 5) { $('#over').goTo(); console.log("nu" + ($('#over').offsetTop()));}
-    //if (scroll == ($('#over').scrollTop() + 5)) { $('#Parcours').goTo(); }
-    //$('#over').goTo();
 }
 
 
@@ -60,31 +54,15 @@ function ScrollNaarParcours() {
 
 
 function LoadImages(object) {
-    var fileNames = ["Parcours_1913","Parcours_19132","Parcours_19133","Parcours_19134"];
+    var fileNames = ["Parcours_1913","Parcours_1939","Parcours_1969","Parcours_1977","Parcours_1998","Parcours_2010","Parcours_2012"];
     var dir = "/images/Parcours/";
     var firstImg = $('#parcourImg');
     
     $("#controls span").removeClass("selected");
     $(object).addClass("selected");
-    
-    console.log(dir + $(object).index() + ".jpg");
 
     firstImg.fadeOut(800, function () {
-        firstImg.attr("src", dir + fileNames[$(object).index()] + ".png");
+        firstImg.attr("src", dir + fileNames[$(object).index()] + ".jpg");
         firstImg.fadeIn(800);
     });
-    
-    /*if ($(firstImg).is(":visible")) {
-        //$("#parcourImg").attr("src", dir + "pony.jpg");
-        secondImg.css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0, src: "images/Parcours/wiel.jpg" }, 1000);
-        firstImg.css({ opacity: 1.0, visibility: "visible" }).animate({ opacity: 0 }, 1000);
-        
-    }
-    else {
-        firstImg.css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 }, 1000);
-        secondImg.css({ opacity: 1.0, visibility: "visible" }).animate({ opacity: 0, visibility: "hidden" }, 1000);
-        
-    }*/
-    
-    
 }
