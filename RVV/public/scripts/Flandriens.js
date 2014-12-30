069b6c3d0bb377981d3a2874fc499f6bb5a11bf7
AshimDeCoster <ashim.de.coster@student.howest.be>
2014-12-30 16:38:05 +0100

    text boven img

--- RVV/public/scripts/Flandriens.js
+++ RVV/public/scripts/Flandriens.js
@@ -3,22 +3,36 @@
     VeranderFoto();
 });
 var i = 1;
+var myTimeOut;
 function VeranderFoto() {
-    var randomImg = Math.floor(Math.random() * (4 - 1) + 1);
-    var img = $("#foto"+ i);
+    //var randomImg = Math.floor(Math.random() * (4 - 1) + 1);
+    var img = $("#foto" + i);
+    var span = $("#span" + i);
     var dir = "/images/Flandriens/";
     
     var flandrien = $.parseJSON($('#flandriens').val());
     var random = Math.floor(Math.random() * (13 - 0) + 0);
-    
+    var src1 = $("#foto1").attr('src');
+    var src2 = $("#foto2").attr('src');
+    var src3 = $("#foto3").attr('src');
+    while(src1 == dir + flandrien[random].img || src2 == dir + flandrien[random].img || src3 == dir + flandrien[random].img) {
+        random = Math.floor(Math.random() * (13 - 0) + 0);
+    }
     img.fadeOut(800, function () {
-        img.attr("src", dir + flandrien[random].img);
-        img.fadeIn(800);
-    });
+           span.fadeOut(400, function () {
+            span.text(flandrien[random].naam);
+            span.fadeIn(400);
+            });
+            img.attr("src", dir + flandrien[random].img);
+            
+            img.fadeIn(800);
+        });
+  
+    
     if (i < 4)
         i++;
     else
         i = 1;
-    setTimeout("VeranderFoto()", 3000);
+    myTimeOut =setTimeout("VeranderFoto()", 3000);
 }
 
