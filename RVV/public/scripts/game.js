5c7c7c72b4bb112a5068d0d2525e7a93ee66e830
glennVw <glenn.vanwynsberghe@student.howest.be>
2015-01-02 12:06:10 +0100

    socket.io add

--- RVV/public/scripts/game.js
+++ RVV/public/scripts/game.js
@@ -1,38 +1,25 @@
 ﻿/**************************************************
 ** GAME VARIABLES
 **************************************************/
-var canvas,			// Canvas DOM element
-    ctx,			// Canvas rendering context
-    keys,			// Keyboard input
-    localPlayer;	// Local player
-
+var localPlayer,	// Local player
+    socket;
 
 /**************************************************
 ** GAME INITIALISATION
 **************************************************/
+$(document).ready(function () {
+    init();
+    console.log("test");
+});
+
 function init() {
-    // Declare the canvas and rendering context
-    canvas = document.getElementById("gameCanvas");
-    ctx = canvas.getContext("2d");
     
-    // Maximise the canvas
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
     
-    // Initialise keyboard controls
-    keys = new Keys();
-    
-    // Calculate a random start position for the local player
-    // The minus 5 (half a player size) stops the player being
-    // placed right on the egde of the screen
-    var startX = Math.round(Math.random() * (canvas.width - 5)),
-        startY = Math.round(Math.random() * (canvas.height - 5));
-    
-    // Initialise the local player
-    localPlayer = new Player(startX, startY);
+    localPlayer = new Player(0, 0);
     
     // Start listening for events
     setEventHandlers();
+    socket = io.connect("http://localhost", { port: 8000, transports: ["websocket"] });
 };
 
 
@@ -40,63 +27,29 @@ function init() {
 ** GAME EVENT HANDLERS
 **************************************************/
 var setEventHandlers = function () {
-    // Keyboard
-    window.addEventListener("keydown", onKeydown, false);
-    window.addEventListener("keyup", onKeyup, false);
-    
-    // Window resize
-    window.addEventListener("resize", onResize, false);
+    socket.on("connect", onSocketConnected);
+    socket.on("disconnect", onSocketDisconnect);
+    socket.on("new player", onNewPlayer);
+    socket.on("move player", onMovePlayer);
+    socket.on("remove player", onRemovePlayer);
 };
 
-// Keyboard key down
-function onKeydown(e) {
-    if (localPlayer) {
-        keys.onKeyDown(e);
-    }    ;
+function onSocketConnected() {
+    console.log("Connected to socket server");
 };
 
-// Keyboard key up
-function onKeyup(e) {
-    if (localPlayer) {
-        keys.onKeyUp(e);
-    }    ;
+function onSocketDisconnect() {
+    console.log("Disconnected from socket server");
 };
 
-// Browser window resize
-function onResize(e) {
-    // Maximise the canvas
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+function onNewPlayer(data) {
+    console.log("New player connected: " + data.id);
 };
 
+function onMovePlayer(data) {
 
-/**************************************************
-** GAME ANIMATION LOOP
-**************************************************/
-function animate() {
-    update();
-    draw();
-    
-    // Request a new animation frame using Paul Irish's shim
-    window.requestAnimFrame(animate);
 };
 
+function onRemovePlayer(data) {
 
-/**************************************************
-** GAME UPDATE
-**************************************************/
-function update() {
-    localPlayer.update(keys);
-};
-
-
-/**************************************************
-** GAME DRAW
-**************************************************/
-function draw() {
-    // Wipe the canvas clean
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    // Draw the local player
-    localPlayer.draw(ctx);
 };
