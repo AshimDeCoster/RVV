3abeffaddd65368e6e2781b374c068b84bd18549
glennVw <glenn.vanwynsberghe@student.howest.be>
2015-01-05 12:07:41 +0100

    aanpassingen spel

--- RVV/public/scripts/game.js
+++ RVV/public/scripts/game.js
@@ -1,43 +1,29 @@
 ﻿var localPlayer,
     remotePlayers,
-    socket, opponent;
+    socket,
+    opponent;
 
 $(document).ready(function () {
-    init();    
+    init();
     $('#play').prop('disabled', true);
-    
    
 });
-<<<<<<< HEAD
-var btnTest = $('#test');
-btnTest.click(function () {
-    //localPlayer.setX((localPlayer.getX() + 1));
-    //socket.emit("move player", { x: localPlayer.getX(), id: localPlayer.id });
-    
-    if (opponent === undefined) {
-        
-        alert("please wait");
-    }
-    else {
-        alert(opponent.id);
-=======
 
 $('#play').click(function () {
-   
-   
+    
+    console.log("test");
     if (opponent === undefined || opponent == null) {
         alert("please wait");
     }
     else {
         $("html, body").animate({ scrollTop: $(document).height() }, 1000);
         socket.emit("race", { id: localPlayer.id, opp: opponent.id });
->>>>>>> 1da66ca3eba1d062a06278643c5c8a75f3610b98
     }
 
 });
 $('#speler1').click(function () {
-   
-    if ($('#btnSpeler2').val() == "Start !!") {
+    
+    if ($('#btnSpeler2').val() == "START!") {
         console.log(typeof (localPlayer));
         localPlayer.setX((localPlayer.getX() + 1));
         socket.emit("move player", { x: localPlayer.getX(), id: localPlayer.id, opp: opponent.id });
@@ -45,27 +31,27 @@ $('#speler1').click(function () {
 });
 
 window.onbeforeunload = function () {
-console.log(localPlayer.getID());
+    console.log(localPlayer.getID());
     socket.emit("remove player", { id: localPlayer.id });
 };
 
 function init() {
-       
+    
     //canvas = document.getElementById("gameCanvas");
     //ctx = canvas.getContext("2d");
-    localPlayer = new Player(4);    
+    localPlayer = new Player(4);
     // Start listening for events   
-     socket = io.connect("http://178.116.189.17:1338");  
+    socket = io.connect("http://178.116.189.17:1338");
     //socket = io.connect();  
     setEventHandlers();
-    remotePlayers = [];    
+    remotePlayers = [];
 };
 
-var setEventHandlers = function() {	
-	socket.on("connect", onSocketConnected);
-	socket.on("disconnect", onSocketDisconnect);
-	socket.on("new player", onNewPlayer);
-	socket.on("move player", onMovePlayer);
+var setEventHandlers = function () {
+    socket.on("connect", onSocketConnected);
+    socket.on("disconnect", onSocketDisconnect);
+    socket.on("new player", onNewPlayer);
+    socket.on("move player", onMovePlayer);
     socket.on("remove player", onRemovePlayer);
     socket.on("global player", onNewPlayerGlobal);
     socket.on("set opponent", onSetOpponent);
@@ -78,30 +64,33 @@ function onEndGame(data) {
         alert("je bent gewonnen");
         opponent = null;
         $('#speler1').animate({ bottom: '4%' }, 50);
-        $('#speler2').animate({ bottom: '4%' }, 50); 
+        $('#speler2').animate({ bottom: '4%' }, 50);
     }
     else if (data.isWon == false) {
         alert("je hebt verloren");
         opponent = null;
         $('#speler1').animate({ bottom: '4%' }, 50);
-        $('#speler2').animate({ bottom: '4%' }, 50); 
-    } else { } 
+        $('#speler2').animate({ bottom: '4%' }, 50);
+    } else { }
+    localPlayer.setX(4);
+    $("#btnSpeler2").prop('value', "Wachten op tegenstander...");
+    socket.emit("remove player", { id: localPlayer.id });
+    socket.emit("new player", { x: localPlayer.getX() });
 }
 function onRace(data) {
     console.log(data.id + " is ready to race");
-    $("#btnSpeler2").prop('value', "Start !!");
-   
+    $("#btnSpeler2").prop('value', "START!");
 }
 function onSetOpponent(data) {
     var play = new Player(4);
-    play.id = data.id;    
+    play.id = data.id;
     opponent = play;
     $('#play').prop('disabled', false);
 }
 
 function onSocketConnected() {
     console.log("Connected to socket server");
-   
+    
     socket.emit("new player", { x: localPlayer.getX() });
 };
 
@@ -110,12 +99,12 @@ function onSocketDisconnect() {
     socket.emit("remove player", { id: localPlayer.getID() });
 };
 
-function onNewPlayer(data) {   
+function onNewPlayer(data) {
     
     var newPlayer = new Player(data.x);
     newPlayer.id = data.id;
     
-    console.log("New player connected: " + data.id + "   " + localPlayer.id);    
+    console.log("New player connected: " + data.id + "   " + localPlayer.id);
     localPlayer = newPlayer;
     remotePlayers.push(newPlayer);
     console.log(localPlayer.id);
@@ -128,21 +117,14 @@ function onNewPlayerGlobal(data) {
     newPlayer.id = data.id;
     remotePlayers.push(newPlayer);
     console.log("New Global player connected: " + data.id);
-<<<<<<< HEAD
-    if (opponent === undefined) {
-        opponent = newPlayer;
-        console.log("heel lalala " + opponent.id);
-    }
-=======
     if (opponent === undefined || opponent == null) {
         opponent = newPlayer;
-        socket.emit("player ready", { id: opponent.id, myId: localPlayer.id  });        
+        socket.emit("player ready", { id: opponent.id, myId: localPlayer.id });
         remotePlayers[playerById(opponent.id)].setReady(false);
         remotePlayers[playerById(localPlayer.id)].setReady(false);
         $('#play').prop('disabled', false);
     }
         
->>>>>>> 1da66ca3eba1d062a06278643c5c8a75f3610b98
    
 };
 
@@ -151,20 +133,21 @@ function onMovePlayer(data) {
     var positie = $("body").scrollTop();
     if (data.id == localPlayer.id) {
         if (data.x <= 71) {
-            $("html, body").animate({ scrollTop: (positie - 50) }, 50);
-            $('#speler1').animate({ bottom: data.x + "%" }, 50);            
+            $("html, body").animate({ scrollTop: (positie - (($('body').height() - window.innerHeight) * 0.007)) }, 50);
+            console.log(positie - (($('body').height() - window.innerHeight) * 0.007));
+            $('#speler1').animate({ bottom: data.x + "%" }, 50);
         }
-        else {          
+        else {
             socket.emit("end game", { win: data.id, los: opponent.id });
         }
     }
     else if (data.id == opponent.id) {
         if (data.x <= 71) {
-            $("html, body").animate({ scrollTop: (positie - 50) }, 50);
+            //$("html, body").animate({ scrollTop: (positie - 50) }, 50);
             $('#speler2').animate({ bottom: data.x + "%" }, 50);
         }
-        else {       
-            socket.emit("end game", { win: data.id, los: localPlayer.id  });
+        else {
+            socket.emit("end game", { win: data.id, los: localPlayer.id });
         }
     } else { }
 };
@@ -181,7 +164,7 @@ function onRemovePlayer(data) {
             }
         }
         return;
-    };
+    }    ;
     if (typeof (opponent) != undefined && opponent != null) {
         if (opponent.id == data.id) {
             opponent = null;
@@ -191,6 +174,7 @@ function onRemovePlayer(data) {
     remotePlayers.splice(remotePlayers.indexOf(removePlayer), 1);
     console.log("removed " + remotePlayers.length);
 };
+
 function playerById(id) {
     var i;
     for (i = 0; i < remotePlayers.length; i++) {
@@ -200,4 +184,3 @@ function playerById(id) {
     
     return false;
 };
-
